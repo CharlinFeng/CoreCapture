5228f27bcae6369f4fad0cd3987daa9987e92092
时点软件 冯成林 <Charlin@fengchenglindeiMac.local>
2017-06-16 16:37:51 +0800

    update

---
+++ CoreCapture/CoreCapture/CoreCaptureHeader.h
@@ -0,0 +1,16 @@
+//
+//  CoreCaptureHeader.h
+//  CoreCapture
+//
+//  Created by 冯成林 on 2017/6/16.
+//  Copyright © 2017年 冯成林. All rights reserved.
+//
+
+#ifndef CoreCaptureHeader_h
+#define CoreCaptureHeader_h
+
+#import "RCMediaViewController.h"
+#import "RCMediaViewController+Handle.h"
+
+#endif 
+/* CoreCaptureHeader_h */
